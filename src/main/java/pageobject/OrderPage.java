11af0251ddadf4ffbcccd912e131c7c8671365ee
Vyacheslav Saburov <v.a.saburov@yandex.com>
2024-10-28 19:28:03 +0300

    project publishing

---
+++ src/main/java/pageobject/OrderPage.java
@@ -0,0 +1,123 @@
+package pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Objects;
+
+public class OrderPage {
+    private WebDriver driver;
+
+    //Локаторы формы "для кого самокат"
+    // Локатор для поля "Имя"
+    private final By inputName = By.xpath(".//input[contains(@placeholder,'Имя')]");
+    // Локатор для поля "Фамилия"
+    private final By inputSurname = By.xpath(".//input[contains(@placeholder,'Фамилия')]");
+    // Локатор для поля "Адрес"
+    private final By inputAddress = By.xpath(".//input[contains(@placeholder,'Адрес')]");
+    // Локатор для поля "Станция метро"
+    private final By inputMetro = By.xpath(".//input[contains(@placeholder,'Станция')]");
+    //Локатор для выпадающего списка
+    private final By stationMetro = By.className("select-search__select");
+    // Локатор для поля "Телефон"
+    private final By inputPhone = By.xpath(".//input[contains(@placeholder,'Телефон')]");
+    //Локатор для кнопки "Далее"
+    private final By nextButton = By.xpath(".//button[@class = 'Button_Button__ra12g Button_Middle__1CSJM']");
+
+    //Локаторы для формы "Про аренду".
+    //Поле даты.
+    private final By inputDate = By.xpath(".//input[contains(@placeholder,'Когда')]");
+    private final By selectedDay = By.xpath(".//div[contains(@class, 'react-datepicker__day--selected')]");
+    //Локаторы для выпадающего списка - период
+    private final By rentPeriodSelect = By.xpath(".//div[text()='* Срок аренды']");
+    private final String rentPeriodPattern = ".//div[@class='Dropdown-option' and text()='%s']";
+    //Локаторы для выбора цвета
+    private final By greyLabel = By.xpath(".//input[@id = 'grey']");
+    private final By blackLabel = By.xpath(".//input[@id = 'black']");
+    //Локатор для поля комментарий
+    private final By inputNote = By.xpath(".//input[contains(@placeholder,'Комментарий')]");
+
+    //Лакатор для кнопки "Да"
+    private final By yesButton = By.xpath(".//button[text()='Да']");
+    //Локатор для кнопки "Посмотреть статус"
+    private final By statusButton = By.xpath(".//button[text()='Посмотреть статус']");
+    //Локатор для блока с информацией о заказе
+    private final By trackOrderInfo = By.xpath(".//div[contains(@class, 'Track_OrderInfo')]");
+    //Локатор для модального окна подтверждения заказа
+    private final By statusOrder = By.xpath(".//div[contains(@class, 'Order_ModalHeader')]");
+    //Локатор для логотипа самокат.
+    private final By scooterLogo = By.xpath(".//a[contains(@class, 'Header_LogoScooter')]");
+    //Конструктор класса
+    public OrderPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Заполнить поля формы "Для кого самокат"
+    public void spellingField(String name, String surname, String address, String metro, String phone) {
+        driver.findElement(inputName).sendKeys(name);
+        driver.findElement(inputSurname).sendKeys(surname);
+        driver.findElement(inputAddress).sendKeys(address);
+        driver.findElement(inputMetro).sendKeys(metro);
+        driver.findElement(stationMetro).click();
+        driver.findElement(inputPhone).sendKeys(phone);
+    }
+
+    public void clickNextButton() {
+        driver.findElement(nextButton).click();
+    }
+
+    //Ввести дату и комментарий
+    public void inputField(String date, String note) {
+        driver.findElement(inputDate).sendKeys(date);
+        //Кликнуть по выбранной дате, чтобы закрыть выпадающий список с календарем.
+        driver.findElement(selectedDay).click();
+        driver.findElement(inputNote).sendKeys(note);
+
+    }
+
+    //Выбрать период
+    public void selectRentPeriod(String period) {
+        driver.findElement(rentPeriodSelect).click();
+        driver.findElement(By
+                .xpath(String.format(rentPeriodPattern, period))).click();
+    }
+
+    //Выбрать цвет самоката
+    public void selectColor(String color) {
+        if (Objects.equals(color, "чёрный жемчуг")) {
+            driver.findElement(blackLabel).click();
+        }
+        if (Objects.equals(color, "серая безысходность")) {
+            driver.findElement(greyLabel).click();
+        }
+    }
+    //Кликнуть на кнопку да.
+    public void clickOnYesButton() {
+        new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.visibilityOfElementLocated(yesButton));
+        driver.findElement(yesButton).click();
+    }
+    //Прочитать заголовок на всплывающем окне "Статус заказа"
+    public String readOrderStatus() {
+        return driver.findElement(statusOrder).getText();
+    }
+    //Кликнуть на кнопку "Посмотреть статус"
+    public void clickOnStatusButton() {
+        new WebDriverWait(driver, Duration.ofSeconds(15))
+                .until(ExpectedConditions.visibilityOfElementLocated(statusButton));
+        driver.findElement(statusButton).click();
+    }
+    //Ожидание загрузки страницы с информацией о заказе
+    public void waitPageLoad() {
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(trackOrderInfo));
+    }
+    //Кликнуть по логотипу самокат, что бы вернутся на главную страницу
+    public void clickOnLogo() {
+        driver.findElement(scooterLogo).click();
+    }
+}
+
---
+++ src/test/java/sprint4/OrderTest.java
@@ -0,0 +1,109 @@
+package sprint4;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import pageobject.MainPage;
+import pageobject.OrderPage;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+@RunWith(Parameterized.class)
+public class OrderTest {
+    private static WebDriver driver;
+
+    private final String name;
+    private final String surname;
+    private final String address;
+    private final String metro;
+    private final String phone;
+    private final String date;
+    private final String period;
+    private final String color;
+    private final String note;
+
+    public OrderTest(String name, String surname, String address, String metro, String phone,
+                     String date, String period, String color, String note) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.metro = metro;
+        this.phone = phone;
+        this.date = date;
+        this.period = period;
+        this.color = color;
+        this.note = note;
+
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] orderInfo() {
+        return new Object[][]{
+                {"Незнайка", "Солнечный", "ул.Лунная, 15", "Лубянка", "+79997775533", "27.12.2024", "двое суток", "серая безысходность", "Без комментариев!"},
+                {"Мурлыка", "Сиамский", "ул.Крылатские коты, 13", "Воробьёвы горы", "+79957735593", "16.11.2024", "семеро суток", "чёрный жемчуг", "У двери мяукнуть, а не царапать!"}
+        };
+    }
+    @Before
+    public void startActivity() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
+        driver = new FirefoxDriver(options);
+        driver.get("https://qa-scooter.praktikum-services.ru");
+    }
+    @Test
+    public void checkOrder() {
+        //Создание объекта класса главной страницы
+        MainPage objMainPage = new MainPage(driver);
+        //Создание объекта класса страницы заказа
+        OrderPage objOrderPage = new OrderPage(driver);
+        //Ожидание загрузки главной страницы
+        objMainPage.waitPageLoad();
+        //Подтвердить согласие на использование куки
+        objMainPage.clickConfirmButton();
+        //Кликнуть по кнопке "Заказать" в центре страницы
+        objMainPage.clickOnMiddleOrderButton();
+        //Заполняем поля имя, фамилия, адрес, метро, телефон
+        objOrderPage.spellingField(name, surname, address, metro, phone);
+        //Кликнуть по кнопке "Далее"
+        objOrderPage.clickNextButton();
+        //Ввести дату и комментарий
+        objOrderPage.inputField(date, note);
+        //Выбрать период
+        objOrderPage.selectRentPeriod(period);
+        //Выбрать цвет самоката
+        objOrderPage.selectColor(color);
+        //Кликнуть на кнопку "Заказать" ниже заполненной формы. При использовании кнопки "Заказать"
+        //сверху формы тест падает. Выявлен баг.
+        objMainPage.clickOnMiddleOrderButton();
+        //Кликнуть на кнопку "Да".
+        objOrderPage.clickOnYesButton();
+        //Проверить, что текст в окне содержит текст "Заказ оформлен"
+        MatcherAssert.assertThat("Заказ не оформлен", objOrderPage.readOrderStatus(), containsString("Заказ оформлен"));
+        //Кликнуть на кнопку "Посмотреть статус"
+        objOrderPage.clickOnStatusButton();
+        //Ожидание загрузки страницы с информацией о заказе
+        objOrderPage.waitPageLoad();
+        //Кликнуть по логотипу самокат, что бы вернутся на главную страницу.
+        objOrderPage.clickOnLogo();
+        //Ожидание загрузки главной страницы
+        objMainPage.waitPageLoad();
+        //Кликнуть по кнопке "Заказать" сверху страницы
+        objMainPage.clickOnTopOrderButton();
+        //Для того чтобы убедится что кнопка "Заказать" работает, заполняем ещё раз поля формы "Для кого самокат"
+        objOrderPage.spellingField(name, surname, address, metro, phone);
+
+
+    }
+
+  @After
+    public void tearDown() {
+        // Закрой браузер
+        driver.quit();
+    }
+}
